80384cc1637b2f3d231a3e0a6c8febdabdc963bd
FREDERIK DIAMOND <hello@frederikhandberg.com>
2025-04-08 16:16:52 +0200

    Now successfully publish to GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), ghPages()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   base: "/pokemon-project/",
 });
